54342a2bbfe416d074d545412cc1cb586f407f55
poorva-1 <poorvamohoni@gmail.com>
2019-08-13 11:58:56 +0530

    Updates

---
+++ precache-manifest.c2c34512c5e2bfe146c6549d15030d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b12e3203f6297288263df1105c09148",
+    "url": "/reactproject/index.html"
+  },
+  {
+    "revision": "33bb8ca82e7f20be92ed",
+    "url": "/reactproject/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6249a58e49546bd98c50",
+    "url": "/reactproject/static/js/2.f1ab4a22.chunk.js"
+  },
+  {
+    "revision": "33bb8ca82e7f20be92ed",
+    "url": "/reactproject/static/js/main.38621688.chunk.js"
+  },
+  {
+    "revision": "31c15a2f379b41373345",
+    "url": "/reactproject/static/js/runtime~main.50961f9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
